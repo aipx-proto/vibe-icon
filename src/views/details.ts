bd36f03c0aa2dc5baf13787ab96590b3207ac546
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2025-06-15 09:20:09 -0700

    update html

--- src/views/details.ts
+++ src/views/details.ts
@@ -19,38 +19,47 @@ export function renderDetails(icon: SearchResult, detailsContainer: HTMLElement)
         ${icon.options.map(
           (option) => html`
             <div class="style-section">
+              <h3>${option.style}</h3>
               <div class="icon-preview">
                 <svg width="96" height="96">
                   <use href="${import.meta.env.BASE_URL}/${icon.filename}#${option.style}" />
                 </svg>
               </div>
-
-              <div class="code-snippet">
-                <h4>${option.style}</h4>
-                <p>1. Add to index.html:</p>
-                <code-snippet
-                  .lang=${"html"}
-                  .code=${`
-<svg style="display: none;">
-  <symbol id="${icon.filename.split(".svg")[0]}-${option.style}">
-    <!-- Icon content from ${icon.filename}#${option.style} -->
-  </symbol>
-</svg>
-                  `.trim()}
-                ></code-snippet>
-                <p>2. Use the icon:</p>
-                <code-snippet
-                  .lang=${"html"}
-                  .code=${`
-<svg width="24" height="24">
-  <use href="#${icon.filename.split(".svg")[0]}-${option.style}" />
-</svg>
-                    `.trim()}
-                ></code-snippet>
-              </div>
             </div>
           `
         )}
+        <section class="code-snippet">
+          <h3>Advanced</h3>
+          <p>1. Add to index.html:</p>
+          <code-snippet
+            .lang=${"html"}
+            .code=${`
+<svg style="display: none;">
+
+  <!-- Other icons code -->
+
+${icon.options
+  .map(
+    (option) => `  <symbol id="${icon.filename.split(".svg")[0]}-${option.style}">
+    <!-- Icon content -->
+  </symbol>`
+  )
+  .join("\n\n")}
+</svg>
+            `.trim()}
+          ></code-snippet>
+          <p>2. Use the icon:</p>
+          <code-snippet
+            .lang=${"html"}
+            .code=${icon.options
+              .map(
+                (option) => `<svg width="24" height="24">
+  <use href="#${icon.filename.split(".svg")[0]}-${option.style}" />
+</svg>`
+              )
+              .join("\n\n")}
+          ></code-snippet>
+        </section>
       </div>
     `,
     detailsContainer
